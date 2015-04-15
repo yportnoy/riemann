9b9f9f99580d55d344ccf1bdf2f3f9f417333aa5
Yasha Portnoy <yportnoy@optoro.com>
2015-04-15 16:32:53 -0400

    Default jdk version should be 7

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,4 +7,4 @@ default[:riemann][:riemann_tarball_location] = "http://aphyr.com/riemann/riemann
 default[:riemann][:riemann_install_location] = "/opt/riemann"
 default[:riemann][:riemann_tarball] = "riemann-0.2.0.tar.bz2"
 default[:riemann][:riemann_version] = "riemann-0.2.0"
-default[:riemann][:jdk_package] = "openjdk-8-jdk"
+default[:riemann][:jdk_package] = "openjdk-7-jdk"
